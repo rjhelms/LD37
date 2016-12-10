49c88fb19ab26c190d0d0b191c0a4eb9a441e236
Rob Hailman <robhailman@gmail.com>
2016-12-10 14:01:25 -0500

    Add relation to name generator

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -10,11 +10,15 @@ public class GameController : MonoBehaviour
     public TextAsset NamesFemale;
     public TextAsset NamesSurnames;
 
+    public TextAsset RelationsMale;
+    public TextAsset RelationsFemale;
+
     public Text NameText;
+
     // Use this for initialization
     void Start()
     {
-
+        NameText.text = GenerateName();
     }
 
     // Update is called once per frame
@@ -30,9 +34,13 @@ public class GameController : MonoBehaviour
     {
         string first_name;
         string last_name;
+        string relation;
+
         string[] first_name_list;
         string[] last_name_list = NamesSurnames.text.Split(
             new[] { "\r\n", "\r", "\n" }, System.StringSplitOptions.None);
+        string[] relation_list;
+
         bool male = false;
         if (Random.value < 0.5)
         {
@@ -43,16 +51,21 @@ public class GameController : MonoBehaviour
             Debug.Log("male");
             first_name_list = NamesMale.text.Split(new[] { "\r\n", "\r", "\n" },
                                              System.StringSplitOptions.None);
+            relation_list = RelationsMale.text.Split(
+                new[] { "\r\n", "\r", "\n" }, System.StringSplitOptions.None);
         }
         else
         {
             Debug.Log("female");
             first_name_list = NamesFemale.text.Split(
                 new[] { "\r\n", "\r", "\n" }, System.StringSplitOptions.None);
+            relation_list = RelationsFemale.text.Split(
+                new[] { "\r\n", "\r", "\n" }, System.StringSplitOptions.None);
         }
         first_name = first_name_list[Random.Range(0, first_name_list.Length)];
         last_name = last_name_list[Random.Range(0, last_name_list.Length)];
-
-        return string.Format("{0}\r\n{1}", first_name, last_name);
+        relation = relation_list[Random.Range(0, relation_list.Length)];
+        return string.Format("{0} {1}, {2}", first_name, last_name,
+                             relation);
     }
 }
